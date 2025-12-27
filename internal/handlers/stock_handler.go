d0316c22844c1e55627da7a34a5c4db424e40907
rohan <gargrohan138@gmail.com>
2025-12-28 01:45:47 +0530

    adding price parameter in stock handler and /create-stock route

--- internal/handlers/stock_handler.go
+++ internal/handlers/stock_handler.go
@@ -1,15 +1,20 @@
 package handlers
 
 import (
+	"time"
+
 	"github.com/Sandstorm831/021_trade/internal/database"
 	"github.com/Sandstorm831/021_trade/internal/models"
 	"github.com/gin-gonic/gin"
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
 )
 
 func CreateStock(c *gin.Context) {
 	var stockName struct {
 		Symbol string
 		Name   string
+		Price  decimal.Decimal
 	}
 	if err := c.ShouldBindJSON(&stockName); err != nil {
 		c.JSON(400, gin.H{
@@ -18,14 +23,27 @@ func CreateStock(c *gin.Context) {
 		return
 	}
 	stock := models.Stock{Symbol: stockName.Symbol, Name: stockName.Name}
-	result := database.DB.Create(&stock)
-	if result.Error != nil {
-		c.JSON(400, gin.H{
-			"message": "some error while creating stock in database",
-		})
-	}
+	stockPrice := models.StockPrice{StockSymbol: stockName.Symbol, PriceINR: stockName.Price, CapturedAt: time.Now()}
+	database.DB.Transaction(func(tx *gorm.DB) error {
+		stockRes := database.DB.Create(&stock)
+		if stockRes.Error != nil {
+			c.JSON(400, gin.H{
+				"message": "some error while creating stock in database",
+			})
+			return stockRes.Error
+		}
+		priceRes := database.DB.Create(&stockPrice)
+		if priceRes.Error != nil {
+			c.JSON(400, gin.H{
+				"message": "some error while inserting stock price in database",
+			})
+			return priceRes.Error
+		}
+		return nil
+	})
 
 	c.JSON(200, gin.H{
 		"Stock": stock,
+		"Price": stockPrice.PriceINR,
 	})
 }
