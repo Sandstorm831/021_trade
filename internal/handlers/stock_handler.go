18e4013e7e1f452ffbd3dedc537e8a5d9f33fca5
rohan <gargrohan138@gmail.com>
2025-12-28 21:40:50 +0530

    adding logging with logrus in stock_handler

--- internal/handlers/stock_handler.go
+++ internal/handlers/stock_handler.go
@@ -7,16 +7,19 @@ import (
 	"github.com/Sandstorm831/021_trade/internal/models"
 	"github.com/gin-gonic/gin"
 	"github.com/shopspring/decimal"
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 func CreateStock(c *gin.Context) {
+	logrus.Info("Entering CreateStock handler.")
 	var stockName struct {
 		Symbol string
 		Name   string
 		Price  decimal.Decimal
 	}
 	if err := c.ShouldBindJSON(&stockName); err != nil {
+		logrus.WithError(err).Error("Failed to bind JSON for CreateStock.")
 		c.JSON(400, gin.H{
 			"message": "Some error occurred",
 		})
@@ -24,16 +27,24 @@ func CreateStock(c *gin.Context) {
 	}
 	stock := models.Stock{Symbol: stockName.Symbol, Name: stockName.Name}
 	stockPrice := models.StockPrice{StockSymbol: stockName.Symbol, PriceINR: stockName.Price, CapturedAt: time.Now()}
+	logrus.WithFields(logrus.Fields{
+		"symbol": stockName.Symbol,
+		"name":   stockName.Name,
+		"price":  stockName.Price,
+	}).Info("Attempting to create new stock and record its price.")
+
 	database.DB.Transaction(func(tx *gorm.DB) error {
-		stockRes := database.DB.Create(&stock)
+		stockRes := tx.Create(&stock)
 		if stockRes.Error != nil {
+			logrus.WithField("stockSymbol", stock.Symbol).WithError(stockRes.Error).Error("Some error occurred while creating stock in database.")
 			c.JSON(400, gin.H{
 				"message": "some error while creating stock in database",
 			})
 			return stockRes.Error
 		}
-		priceRes := database.DB.Create(&stockPrice)
+		priceRes := tx.Create(&stockPrice)
 		if priceRes.Error != nil {
+			logrus.WithField("stockSymbol", stockPrice.StockSymbol).WithError(priceRes.Error).Error("Some error occurred while inserting stock price in database.")
 			c.JSON(400, gin.H{
 				"message": "some error while inserting stock price in database",
 			})
@@ -42,6 +53,7 @@ func CreateStock(c *gin.Context) {
 		return nil
 	})
 
+	logrus.Infof("Successfully created stock %s with price %s.", stock.Symbol, stockPrice.PriceINR)
 	c.JSON(200, gin.H{
 		"Stock": stock,
 		"Price": stockPrice.PriceINR,
