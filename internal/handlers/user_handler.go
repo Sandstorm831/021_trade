6168d17e376264e0c677aa914200cfc1b7c7a2ed
rohan <gargrohan138@gmail.com>
2025-12-28 21:41:26 +0530

    added some extra logs in user hander

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -19,10 +19,14 @@ func RandomString(length int) string {
 }
 
 func CreateUser(c *gin.Context) {
+	logrus.Info("Entering CreateUser handler.")
 	var userName struct {
 		Name string
 	}
 	if err := c.ShouldBindJSON(&userName); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Error("Failed to bind JSON in CreateUser handler.")
 		c.JSON(400, gin.H{
 			"message": "Some error occurred",
 		})
@@ -32,15 +36,19 @@ func CreateUser(c *gin.Context) {
 		userName.Name = RandomString(8)
 	}
 	user := models.User{Name: userName.Name}
-	logrus.Infof("Creating User: %s", user.Name)
+	logrus.Infof("Attempting to create user: %s", user.Name)
 	result := database.DB.Create(&user)
 	if result.Error != nil {
+		logrus.WithFields(logrus.Fields{
+			"user_name": user.Name,
+			"error":     result.Error.Error(),
+		}).Error("Failed to insert user in database.")
 		c.JSON(400, gin.H{
 			"message": "some error while inserting user in database",
 		})
+		return
 	}
-	logrus.Infof("User %s, created successfuly", user.Name)
-	logrus.WithFields(logrus.Fields{"Name": user.Name, "CreatedAt": user.CreatedAt})
+	logrus.WithFields(logrus.Fields{"Name": user.Name, "CreatedAt": user.CreatedAt}).Infof("User %s, created successfully.", user.Name)
 	c.JSON(200, gin.H{
 		"User": user,
 	})
